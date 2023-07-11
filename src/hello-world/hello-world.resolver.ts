e1b435c9ea4292327dfbe6cfb01c6d60eea11163
Guido <38866056+guidoperman1986@users.noreply.github.com>
2023-07-11 12:48:10 -0300

    gql schema and first resolver

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,24 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver, Int, Args } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'Retorna solo hola mundo',
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hello World';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, { name: 'randomFromZeroTo' })
+  getRandomFromZeroTo(
+    @Args('to', { type: () => Int, nullable: true }) to = 6,
+  ): number {
+    return Math.ceil(Math.random() * to);
   }
 }
