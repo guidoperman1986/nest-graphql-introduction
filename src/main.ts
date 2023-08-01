64d840bb160910f26a556e2e228d8941ce98f3ae
Guido <38866056+guidoperman1986@users.noreply.github.com>
2023-08-01 14:20:44 -0300

    cors commented

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.enableCors();
+  /*   app.enableCors({
+    'Apollo-Require-Preflight': true,
+  }); */
 
   app.useGlobalPipes(
     new ValidationPipe({
